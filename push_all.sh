0cd8d8e3dd7872b01ae9237fe84c645f8e9fa7f6
Gu Junhua <jhgu@nao.cas.cn>
2019-12-13 20:06:17 +0800

    modified:   push_all.sh

--- push_all.sh
+++ push_all.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-for i in `git submodule|awk '{print $2}'`
-do
-    cd $i
-    git push
-    git push github
-    cd ..
-done
+git submodule foreach git push
+git submodule foreach git push github
 git push
